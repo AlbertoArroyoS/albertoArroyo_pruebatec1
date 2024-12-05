94690b15c86450d950301e186b0011025824a4db
Alberto Arroyo <albertoarroyo@hotmail.es>
2024-12-05 18:22:16 +0100

    Entidad Empleados con sus atributos privados, constructores, getters y setters, toString, @id autogenerado y @Tempora .Date para la fecha

---
+++ PruebaTecnica1/src/main/java/org/example/logica/Empleado.java
@@ -0,0 +1,97 @@
+package org.example.logica;
+/**
+ *
+ */
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+public class Empleado implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String apellido;
+    private String cargo;
+    private Double salario;
+
+    //Almacenar en la base de datos .DATE, para que solo almacene día, mes y año
+    @Temporal(TemporalType.DATE)
+    private Date fechaInicio;
+
+    // Constructor vacío
+    public Empleado() {
+    }
+
+    // Constructor completo
+    public Empleado(String nombre, String apellido, String cargo, Double salario, Date fechaInicio) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.cargo = cargo;
+        this.salario = salario;
+        this.fechaInicio = fechaInicio;
+    }
+    //Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public Double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    //toString
+    @Override
+    public String toString() {
+        return "Empleado{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", cargo='" + cargo + '\'' +
+                ", salario=" + salario +
+                ", fechaInicio=" + fechaInicio +
+                '}';
+    }
+}
